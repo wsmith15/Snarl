9ebd3e39363da5df2e02fb5eac63249af57b4797
Wyatt Smith <wsmith15@punahou.edu>
2014-02-05 14:23:52 -1000

    Began Maze Generation
    I wrote the code for the basic generation of the boundaries of the
    maze, and then realized that the generation of the block used to create
    the maze needed to be made in the runner class file, so I moved that. I
    also started wrapping my head around the code involved with recursive
    maze generation although this is not reflected in the code… yet.

--- GenerateMaze.java
+++ GenerateMaze.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.BlackWall;
 
 public class GenerateMaze{
+ 
   
-  Scanner input = new Scanner(System.in);
-  
-  public static void generate(){
-    System.out.println("Please Select a Maze Size by entering the corresponding number.");
-    System.out.println("1. Small \n 2. Medium \n 3. Large \n 4. Massive \n 5. INSANE");
-    
-    
-    
-    //formulas go HERE
-    //need to figure out an easy way to make solid blocks of BlackWalls
-    
-    
-    
+  public static void generate(int a){
+    //starting at location (0,a)
+    //recursion goes here
+    //use a bug to run through the maze and the tiles it steps on becomes part of maze?
+    //could make generation easier...
     
     
   }
+  
   
   
   
---
+++ info/gridworld/actor/ImmuneWall.java
@@ -0,0 +1,24 @@
+package info.gridworld.actor;
+import java.awt.Color;
+
+public class ImmuneWall extends Actor
+{
+    //wall used to determine the boundaries of the grid
+    
+    private static final Color DEFAULT_COLOR = Color.BLACK;
+
+    public ImmuneWall()
+    {
+        setColor(DEFAULT_COLOR);
+    }
+
+
+    public ImmuneWall(Color thisColor)
+    {
+        setColor(thisColor);
+    }
+    public void act()
+    {
+    }
+}
+
